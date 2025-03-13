09e01bbb6d274edd50c5488084f74cad092be542
amadesya <154593967+amadesya@users.noreply.github.com>
2025-03-13 14:41:31 +0300

    Update Task4.py

--- LabWork7/Task4.py
+++ LabWork7/Task4.py
@@ -1,4 +1,11 @@
-Написать и протестировать функцию, изменяющую значения
-переданного в нее списка путем умножения каждого из элементов списка на
-переданное в параметрах число. Если второй параметр отсутствует, умножать на -
-1
+def multiply_elements(lst, multiplier=-1):
+    return [x * multiplier for x in lst]
+
+
+lst1 = [1, 2, 3, 4]
+result1 = multiply_elements(lst1, 2)
+print(result1)  # Ожидаемый вывод: [2, 4, 6, 8]
+
+lst2 = [1, 2, 3, 4]
+result2 = multiply_elements(lst2)
+print(result2)  # Ожидаемый вывод: [-1, -2, -3, -4]
